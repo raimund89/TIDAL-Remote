31468b7d5a7ada424cc999bb2645c732ec9a3e03
Raimond Frentrop <raimund89@gmail.com>
2020-08-07 05:21:32 +0200

    Generalize page items

--- app/src/main/java/net/rfrentrop/tidalremote/ui/PageItems.kt
+++ app/src/main/java/net/rfrentrop/tidalremote/ui/PageItems.kt
@@ -1,14 +1,15 @@
 package net.rfrentrop.tidalremote.ui
 
 import android.graphics.Bitmap
-import android.util.Log
 import androidx.compose.Composable
 import androidx.ui.core.ContentScale
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.clickable
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
+import androidx.ui.graphics.RectangleShape
 import androidx.ui.graphics.asImageAsset
 import androidx.ui.layout.Column
 import androidx.ui.layout.aspectRatio
@@ -26,14 +27,59 @@ import org.json.JSONObject
 
 @Composable
 fun PageArtistItem(item: JSONObject) {
+
+    // Construct the artist roles
+    val roles = ArrayList<String>()
+    for (i in 0 until (item["artistRoles"] as JSONArray).length())
+        roles.add(item.getJSONArray("artistRoles").getJSONObject(i)["category"] as String)
+
+    PageItem(
+        imageUrl = item.getString("picture"),
+        rounded = true,
+        text1 = item["name"] as String,
+        text2 = roles.joinToString(", "),
+        onClick = {
+
+        }
+    )
+}
+
+@Composable
+fun PageAlbumItem(item: JSONObject) {
+
+    // Construct the artist list
+    val artists = ArrayList<String>()
+    for(i in 0 until (item["artists"] as JSONArray).length())
+        artists.add(item.getJSONArray("artists").getJSONObject(i)["name"] as String)
+
+    PageItem(
+        imageUrl = item.getString("cover"),
+        text1 = item["title"] as String,
+        text2 = artists.joinToString(", "),
+        text3 = if(!item.isNull("releaseDate")) (item["releaseDate"] as String).substring(0, 4) else "",
+        onClick = {
+            // TODO: Implement
+        }
+    )
+}
+
+@Composable
+fun PageItem(
+    imageUrl: String,
+    rounded: Boolean = false,
+    text1: String,
+    text2: String,
+    text3: String = "",
+    onClick: () -> Unit
+) {
     Column(
-        modifier = Modifier.width(180.dp) + Modifier.padding(end = 20.dp)
+        modifier = Modifier.width(180.dp) + Modifier.padding(end = 20.dp) + Modifier.clickable(onClick = {onClick()}),
     ) {
-        val loadPictureState = loadPicture(TidalManager.IMAGE_URL.format(item.getString("picture").replace("-", "/"), 320, 320))
+        val loadPictureState = loadPicture(TidalManager.IMAGE_URL.format(imageUrl.replace("-", "/"), 320, 320))
 
         if (loadPictureState is UiState.Success<Bitmap>)
             Surface(
-                shape = RoundedCornerShape(50)
+                shape = if(rounded) RoundedCornerShape(50) else RectangleShape
             ) {
                 Image(
                     modifier = Modifier.aspectRatio(1f),
@@ -49,79 +95,30 @@ fun PageArtistItem(item: JSONObject) {
             )
 
         Text(
-            modifier = Modifier.padding(top = 10.dp),
-            text = item["name"] as String,
-            style = MaterialTheme.typography.body1,
-            color = Color.White,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
-
-        // Construct the artist roles
-        val roles = ArrayList<String>()
-        for (i in 0 until (item["artistRoles"] as JSONArray).length())
-            roles.add(item.getJSONArray("artistRoles").getJSONObject(i)["category"] as String)
-
-        Text(
-            text = roles.joinToString(", "),
-            style = MaterialTheme.typography.body2,
-            color = Color.LightGray,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
-        )
-    }
-}
-
-@Composable
-fun PageAlbumItem(item: JSONObject) {
-    Log.d("PageItems", item.toString())
-    Column(
-        modifier = Modifier.width(180.dp) + Modifier.padding(end = 20.dp)
-    ) {
-        val loadPictureState = loadPicture(TidalManager.IMAGE_URL.format(item.getString("cover").replace("-", "/"), 320, 320))
-
-        if (loadPictureState is UiState.Success<Bitmap>)
-            Image(
-                modifier = Modifier.aspectRatio(1f),
-                asset = loadPictureState.data.asImageAsset(),
-                contentScale = ContentScale.FillWidth
-            )
-        else
-            Image(
-                modifier = Modifier.aspectRatio(1f),
-                asset = imageResource(id = R.drawable.emptycover),
-                contentScale = ContentScale.FillWidth
-            )
-
-        Text(
             modifier = Modifier.padding(top=10.dp),
-            text = item["title"] as String,
+            text = text1,
             style = MaterialTheme.typography.body1,
             color = Color.White,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
 
-        // Construct the artist list
-        val artists = ArrayList<String>()
-        for(i in 0 until (item["artists"] as JSONArray).length())
-            artists.add(item.getJSONArray("artists").getJSONObject(i)["name"] as String)
-
         Text(
-            text = artists.joinToString(", "),
+            text = text2,
             style = MaterialTheme.typography.body2,
             color = Color.LightGray,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
 
-        Text(
-            modifier = Modifier.padding(top=5.dp),
-            text = if(!item.isNull("releaseDate")) (item["releaseDate"] as String).substring(0, 4) else "",
-            style = MaterialTheme.typography.subtitle1,
-            color = MaterialTheme.colors.secondary,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
+        if(text3.isNotEmpty())
+            Text(
+                modifier = Modifier.padding(top=5.dp),
+                text = text3,
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.secondary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
     }
 }
