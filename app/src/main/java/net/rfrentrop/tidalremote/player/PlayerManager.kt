c4a4ebce57bd207f32b57887341dbd829b9ca822
Raimond Frentrop <raimund89@gmail.com>
2020-08-13 01:46:06 +0200

    Initial structure for player websocket handling

--- app/src/main/java/net/rfrentrop/tidalremote/player/PlayerManager.kt
+++ app/src/main/java/net/rfrentrop/tidalremote/player/PlayerManager.kt
@@ -1,6 +1,10 @@
 package net.rfrentrop.tidalremote.player
 
 import net.rfrentrop.tidalremote.MainActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
 import java.net.InetAddress
 
 // TODO: Implement ipv6?
@@ -14,4 +18,30 @@ data class PlayerHost(
 
 class PlayerManager(
         context: MainActivity
-)
+): WebSocketListener(){
+
+        private val client = OkHttpClient()
+        var ws: WebSocket? = null
+
+        fun connectToPlayer(player: PlayerHost) {
+                val request = Request.Builder().url("ws://${player.host.hostAddress}:${player.port}").build()
+                val listener = PlayerSocketListener()
+                ws = client.newWebSocket(request, listener)
+
+                client.dispatcher.executorService.shutdown()
+        }
+
+        fun sendCommand(command: String) {
+                // TODO: Further implement this
+                ws?.send(command)
+        }
+
+        private class PlayerSocketListener: WebSocketListener() {
+
+                override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                        webSocket.close(1000, null)
+                        // TODO: The player is disconnecting for a reason
+                }
+
+        }
+}
